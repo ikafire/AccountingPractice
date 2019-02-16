ec344b62776268d928c7df1f8e91ba58f7df4d84
Cling Lin <clin@bridgewell.com>
2019-02-16 16:54:25 +0800

    [feature] Up until two months

--- AccountingPractice/Accounting.cs
+++ AccountingPractice/Accounting.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 
 namespace AccountingPractice
 {
@@ -13,7 +16,67 @@ namespace AccountingPractice
 
         public double TotalAmount(DateTime start, DateTime end)
         {
-            return 0.0d;
+            List<MonthlyRate> monthlyRates = GetMonthlyRates(start, end);
+            var budgets = BudgetRepo.GetAll();
+            return monthlyRates.Sum(x => CalculateBudget(x, budgets));
+
         }
+
+        private List<MonthlyRate> GetMonthlyRates(DateTime start, DateTime end)
+        {
+
+            if (start.Year == end.Year && start.Month == end.Month)
+            {
+                var yearMonth = ToYearMonth(start);
+                var rate = GetRate(start, end);
+                return  new List<MonthlyRate>(){new MonthlyRate(){Rate = rate, YearMonth = yearMonth}};
+            }
+            else
+            {
+                var startYearMonth = ToYearMonth(start);
+                var startRate = GetRate(start,
+                    new DateTime(start.Year, start.Month, DateTime.DaysInMonth(start.Year, start.Month)));
+
+                var endYearMonth = ToYearMonth(end);
+                var endRate = GetRate(new DateTime(end.Year, end.Month, 1), end);
+
+                return new List<MonthlyRate>()
+                {
+                    new MonthlyRate() { Rate = startRate, YearMonth = startYearMonth },
+                    new MonthlyRate()
+                    {
+                        Rate = endRate,
+                        YearMonth = endYearMonth,
+                    }
+                };
+            }
+        }
+
+        private double CalculateBudget(MonthlyRate monthlyRate, List<Budget> budgets)
+        {
+            return budgets.FirstOrDefault(x => x.YearMonth == monthlyRate.YearMonth)?.Amount* monthlyRate.Rate ?? 0;
+        }
+
+        private static double GetRate(DateTime start, DateTime end)
+        {
+            int monthdays = DateTime.DaysInMonth(start.Year, start.Month);
+            int totalDays = (int) end.Subtract(start).TotalDays + 1;
+            double rate = (double) totalDays / monthdays;
+            return rate;
+        }
+
+        private static string ToYearMonth(DateTime start)
+        {
+            var yearMonth = start.ToString("yyyyMM");
+            return yearMonth;
+        }
+    }
+
+    public class MonthlyRate
+    {
+        public string YearMonth { get; set; }
+        public double Rate { get; set; }
+
+
     }
 }
--- AccountingPractice/AccountingTest.cs
+++ AccountingPractice/AccountingTest.cs
@@ -40,6 +40,67 @@ namespace AccountingPractice
                 0);
         }
 
+        [Fact]
+        public void WholeMonth()
+        {
+            GivenDatabaseHasData(new List<Budget>
+            {
+                new Budget("201901", 31),
+            });
+
+            Accounting = new Accounting(BudgetRepo);
+            TotalAmountShouldBe(
+                new DateTime(2019, 1, 1),
+                new DateTime(2019, 1, 31),
+                31);
+        }
+
+        [Fact]
+        public void MutilDaysInOneMonth()
+        {
+            GivenDatabaseHasData(new List<Budget>
+            {
+                new Budget("201901", 31),
+            });
+
+            Accounting = new Accounting(BudgetRepo);
+            TotalAmountShouldBe(
+                new DateTime(2019, 1, 1),
+                new DateTime(2019, 1, 10),
+                10);
+        }
+
+        [Fact]
+        public void MutilDaysInTwoMonth()
+        {
+            GivenDatabaseHasData(new List<Budget>
+            {
+                new Budget("201901", 31),
+                new Budget("201902",280)
+            });
+
+            Accounting = new Accounting(BudgetRepo);
+            TotalAmountShouldBe(
+                new DateTime(2019, 1, 31),
+                new DateTime(2019, 2, 1),
+                11);
+        }
+
+        [Fact]
+        public void OneDay()
+        {
+            GivenDatabaseHasData(new List<Budget>
+            {
+                new Budget("201901", 31),
+            });
+
+            Accounting = new Accounting(BudgetRepo);
+            TotalAmountShouldBe(
+                new DateTime(2019, 1, 1),
+                new DateTime(2019, 1, 1),
+                1);
+        }
+
         private void TotalAmountShouldBe(DateTime start, DateTime end, double expected)
         {
             Accounting.TotalAmount(start, end).Should().Be(expected);
