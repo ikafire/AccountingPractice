035e48fd9b3af37b0accb02665042fa975c9d3ff
Cling Lin <clin@bridgewell.com>
2019-02-16 15:54:08 +0800

    [refactor]

--- AccountingPractice/AccountingTest.cs
+++ AccountingPractice/AccountingTest.cs
@@ -1,19 +1,55 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
+using Moq;
 using Xunit;
 
 namespace AccountingPractice
 {
     public class AccountingTest
     {
+        private IBudgetRepo BudgetRepo { get; set; }
+
+        private Accounting Accounting { get; set; }
+
         [Fact]
         public void NoData()
         {
-            var emptyBudget = new EmptyBudget();
-            var accounting = new Accounting(emptyBudget);
-            var actual = accounting.TotalAmount(new DateTime(2019, 1, 1), new DateTime(2019, 1, 31));
-            actual.Should().Be(0);
+            GivenDatabaseHasData(new List<Budget>());
+            Accounting = new Accounting(BudgetRepo);
+
+            TotalAmountShouldBe(
+                new DateTime(2019, 1, 1),
+                new DateTime(2019, 1, 31),
+                0);
+        }
+
+        [Fact]
+        public void NoDataInMonth()
+        {
+            GivenDatabaseHasData(new List<Budget>
+            {
+                new Budget("201902", 1),
+            });
+
+            Accounting = new Accounting(BudgetRepo);
+            TotalAmountShouldBe(
+                new DateTime(2019, 1, 1),
+                new DateTime(2019, 1, 31),
+                0);
+        }
+
+        private void TotalAmountShouldBe(DateTime start, DateTime end, double expected)
+        {
+            Accounting.TotalAmount(start, end).Should().Be(expected);
+        }
+
+        private void GivenDatabaseHasData(List<Budget> data)
+        {
+            var budgetRepoMock = new Mock<IBudgetRepo>();
+            budgetRepoMock.Setup(x => x.GetAll()).Returns(data);
+            BudgetRepo = budgetRepoMock.Object;
         }
     }
 }
--- AccountingPractice/Budget.cs
+++ AccountingPractice/Budget.cs
@@ -2,6 +2,12 @@ namespace AccountingPractice
 {
     public class Budget
     {
+        public Budget(string yearMonth, int amount)
+        {
+            YearMonth = yearMonth;
+            Amount = amount;
+        }
+
         public string YearMonth { get; set; }
         public int Amount { get; set; }
     }
